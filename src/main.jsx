875ab6ff07a54981f5a5acbd68f3386eab53dfaf
eddercuero <eddercuero@gmail.com>
2026-08-31 19:30:35 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import EcoRadar from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div style={{ minHeight: "100vh", display: "flex", alignItems: "stretch" }}>
+    <div style={{ minHeight: "100vh", width: "100%", display: "flex", alignItems: "stretch" }}>
       <EcoRadar />
     </div>
   </React.StrictMode>
